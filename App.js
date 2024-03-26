33697f4017554bf914ec428c30083566d23acb76
Deepak/101094 <deepakpandey101094@gmail.com>
2024-03-26 17:53:16 +0530

    create userContext

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import StackNavigator from "./StackNavigator";
+import UserContext from "./UserContext";
 
 export default function App() {
 	return (
 		<>
-			<StackNavigator />
+			<UserContext>
+				<StackNavigator />
+			</UserContext>
 		</>
 	);
 }
